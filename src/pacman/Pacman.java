3b4e4d208c4110f190e7afe296a476f32e33fc2f
Reniery-LB <relu_23@alu.uabcs.mx>
2025-04-10 23:19:29 -0700

    Unidad 3 - Ejercicio 25

--- src/pacman/Pacman.java
+++ src/pacman/Pacman.java
@@ -8,6 +8,7 @@ import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.GridLayout;
 import java.awt.Image;
+import java.awt.Panel;
 import java.awt.Point;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -34,7 +35,8 @@ public class Pacman implements KeyListener{
 	private Player pacman;
 	private List<Player> paredes = new ArrayList<>();
 	Timer timer;
-	private int velocidad = 10;
+	private int lastPress = 0;
+	private int velocidad = 3;
 	/**
 	 * Launch the application.
 	 */
@@ -74,24 +76,24 @@ public class Pacman implements KeyListener{
 		
 		paredes.add(new Player(120, 300, 200, 30, Color.orange));
 		paredes.add(new Player(120, 50, 200, 30, Color.orange));
-		paredes.add(new Player(400, 50, 30, 200, Color.magenta));
+		paredes.add(new Player(380, 90, 30, 200, Color.magenta));
 
 		JPanel pnl_norte = new JPanel();
 		pnl_norte.setBackground(new Color(0, 0, 128));
 		frame.getContentPane().add(pnl_norte, BorderLayout.NORTH);
 		pnl_norte.setLayout(null);
-		pnl_norte.setLayout(new GridLayout(1, 0, 0, 0));
+		pnl_norte.setLayout(new BorderLayout(0,0));
 		pnl_norte.setBorder(BorderFactory.createLineBorder(Color.YELLOW,4));
 		
 		JLabel titulo = new JLabel();
-		titulo.setHorizontalAlignment(JLabel.CENTER);
+		titulo.setHorizontalAlignment(JLabel.RIGHT);
 		ImageIcon logo = new ImageIcon("img/logo.png");
 		Image img = logo.getImage().getScaledInstance(300, 80, Image.SCALE_SMOOTH);
 		ImageIcon icono = new ImageIcon(img);
 		titulo.setIcon(icono);
 		titulo.setForeground(new Color(255, 255, 0));
 		titulo.setFont(new Font("Dialog", Font.BOLD, 18));
-		pnl_norte.add(titulo);
+		pnl_norte.add(titulo, BorderLayout.CENTER);
 		
 		JPanel footer = new JPanel();
 		footer.setBackground(new Color(0, 0, 160));
@@ -135,55 +137,131 @@ public class Pacman implements KeyListener{
 		panelIzq.setBackground(new Color(255, 255, 0));
 		frame.getContentPane().add(panelIzq, BorderLayout.WEST);
 	
-		timer = new Timer(50, new ActionListener() {
+		timer = new Timer(10, new ActionListener() {
 			
 			@Override
 			public void actionPerformed(ActionEvent e) {
-				tablero.repaint();
+				update();
+//				tablero.repaint();
 				
 			}
 		});
 		timer.start();
+		
+		JLabel tiempo = new JLabel("0:00");
+		tiempo.setHorizontalAlignment(JLabel.RIGHT);
+		tiempo.setFont(new Font("Dialog", Font.BOLD, 32));
+		tiempo.setForeground(Color.white);
+		pnl_norte.add(tiempo, BorderLayout.LINE_END);
 	}
 	
-	   class DrawingPanel extends JPanel {
-	        
-	        @Override
-	        protected void paintComponent(Graphics g) {
-	            super.paintComponent(g);
-	            Graphics2D g2d = (Graphics2D) g;
-	            
-	 
-	            g2d.setColor(pacman.c);
-	            g2d.fillOval(pacman.x, pacman.y, pacman.w, pacman.h);
-	            
-	            for(Player pared : paredes) {
-	            	g2d.setColor(pared.c);
-	            	g2d.fillRect(pared.x, pared.y, pared.w, pared.h);  	
-	            }
-	        }
-	    }
+   class DrawingPanel extends JPanel {
+        
+        @Override
+        protected void paintComponent(Graphics g) {
+            super.paintComponent(g);
+            Graphics2D g2d = (Graphics2D) g;
+            
+ 
+            g2d.setColor(pacman.c);
+            g2d.fillOval(pacman.x, pacman.y, pacman.w, pacman.h);
+            
+            for(Player pared : paredes) {
+            	g2d.setColor(pared.c);
+            	g2d.fillRect(pared.x, pared.y, pared.w, pared.h);  	
+            }
+        }
+    }
+	
+	class Player{
 	   
-	   class Player{
-		   
-		   int x,y,w,h;
-		   Color c;
-		   
-		   public Player(int x,int y,int w, int h, Color c) {
-			   this.x = x;
-			   this.y = y;
-			   this.w = w;
-			   this.h = h;
-			   this.c = c;
-		   }
-		   
-		    public boolean colision(Player target) {
-		        return this.x < target.x + target.w &&
-		               this.x + this.w > target.x &&
-		               this.y < target.y + target.h &&
-		               this.y + this.h > target.y;
-		    }
+	   int x,y,w,h;
+	   Color c;
+	   
+	   public Player(int x,int y,int w, int h, Color c) {
+		   this.x = x;
+		   this.y = y;
+		   this.w = w;
+		   this.h = h;
+		   this.c = c;
+	   }
+	   
+	    public boolean colision(Player target) {
+			
+			if(this.x < target.x + target.w && 
+					this.x + this.w > target.x &&
+					this.y < target.y + target.h &&
+					this.y + this.h > target.y) {
+				
+				return true;
+			}	
+			return false;
 		}
+	}
+   
+	public void update() {
+		Boolean colision = false;
+		
+		for (Player pared : paredes) {
+			if(pacman.colision(pared)) {
+				colision = true;
+			}
+		}
+		
+		if(lastPress == 87) { //TECLA W
+			if(!colision) {
+				pacman.y -= velocidad;		
+			}else {
+				pacman.y += velocidad;
+				lastPress = 0;
+			}	
+			//LIMITE DE ARRIBA
+			if (pacman.y <= -30) {
+				pacman.y = 585 ;
+			}
+		} 
+	
+		if(lastPress == 83) { //TECLA S
+			if(!colision) {
+				pacman.y += velocidad;							
+			} else {
+				pacman.y -= velocidad;
+				lastPress = 0;
+			}
+			//LIMITE DE ABAJO
+			if (pacman.y >= 585) {
+				pacman.y = -35;
+			}
+		}
+		
+		if(lastPress == 65) { //TECLA A
+			if(!colision) {
+				pacman.x -= velocidad;							
+			} else {
+				
+				lastPress = 0;
+			}
+			//LIMITE DE LA IZQUIERDA
+			if (pacman.x <= -30) {
+				pacman.x = 445;
+			}
+		}
+			
+		if(lastPress == 68) { //TECLA D
+			if(!colision) {
+				pacman.x += velocidad;				
+			} else {
+				
+				lastPress = 0;
+			}
+			//LIMITE DE LA DERECHA
+			if(pacman.x >= 445) {
+				pacman.x = -25;
+			}
+		}
+		tablero.repaint();
+	}
+	   
 	@Override
 	public void keyTyped(KeyEvent e) {
 //		System.out.println(e.getKeyCode());
@@ -191,98 +269,9 @@ public class Pacman implements KeyListener{
 
 	@Override
 	public void keyPressed(KeyEvent e) {
-//	System.out.println(x);
-		int keyCode = e.getKeyCode();
-		int newX = pacman.x;
-		int newY = pacman.y;
-		
-		switch(keyCode) {
-		case KeyEvent.VK_W:
-			newY -= velocidad;
-			break;
-		case KeyEvent.VK_S:
-			newY += velocidad;
-			break;
-		case KeyEvent.VK_A:
-			newX -= velocidad;
-			break;
-		case KeyEvent.VK_D:
-			newX += velocidad;
-			break;	
-		}
-		
-		Boolean puedeMoverse = true;
-		Player temp_pacman = new Player(newX, newY, pacman.w, pacman.h, pacman.c);
-		
-		for (Player pared: paredes) {
-			if (temp_pacman.colision(pared)) {
-				puedeMoverse = false;
-				break;
-			}
-		}
-		
-		if (puedeMoverse) {
-			pacman.x = newX;
-			pacman.y = newY;
-		}
-		
-       if (pacman.x <= -pacman.w) {
-            pacman.x = tablero.getWidth();
-        } else if (pacman.x >= tablero.getWidth()) {
-            pacman.x = -pacman.w;
-        }
-        
-        if (pacman.y <= -pacman.h) {
-            pacman.y = tablero.getHeight();
-        } else if (pacman.y >= tablero.getHeight()) {
-            pacman.y = -pacman.h;
-        }
-	        
-		
-//		if(e.getKeyCode() == 87) { //TECLA W
-//			if(!colision) {
-//				y-=1;	
-//			}
-//			else {
-//				y+=1;
-//			}
-//			//LIMITE DE ARRIBA
-//			if (y <= -30) {
-//				y =+ 585 ;
-//			}
-//		}
-//		if(e.getKeyCode() == 83) { //TECLA S
-//			if(!colision) {
-//				y+=1;				
-//			}
-//			else {
-//				y-=1;
-//			}
-//			//LIMITE DE ABAJO
-//			if (y >= 585) {
-//				y =+ -35;
-//			}
-//		}
-//		if(e.getKeyCode() == 65) { //TECLA A
-//			if(!colision) {
-//				x-=1;				
-//			}
-//			else {
-//				
-//			}
-//			//LIMITE DE LA IZQUIERDA
-//			if (x <= -30) {
-//				x =+ 445;
-//			}
-//
-//		}
-//		if(e.getKeyCode() == 68) { //TECLA D
-//			x+=1;
-//			//LIMITE DE LA DERECHA
-//			if(x >= 445) {
-//				x =-25;
-//			}
-//		}
+		lastPress = e.getKeyCode();
+		timer.start();
+		update();
 		tablero.repaint();
 	}
 
